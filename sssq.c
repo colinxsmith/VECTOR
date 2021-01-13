8035004604002fc8cec9f560d56fa7fd7feb2295
Colin <colinxsmith@gmail.com>
2021-01-13 21:59:02 +0000

    c verification for dsssq

---
+++ sssq.c
@@ -0,0 +1,32 @@
+
+/* 
+                    Verify conversion from c to c#
+Make on linux with
+gcc -O -I ~/safeqp sssq.c -L ~/safeqp -lsafeqp -o sssq
+
+Result
+./sssq 
+scale=   9.000000000000000, sumsq=   2.037037037037037
+scale=  10.000000000000000, sumsq=   3.850000000000000
+scale=  10.000000000000000, sumsq=   3.850000000000000
+*/
+#include <ldefns.h>
+extern void dsssq(dimen /*n*/, vector /*x*/, increment /*incx*/, real * /*pscale*/, real * /*psumsq*/);
+extern void dsssqvec(dimen /*n*/, vector /*x*/, real * /*pscale*/, real * /*psumsq*/);
+int main()
+{
+    double a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = 10;
+    double scale = 1;
+    double sumsq = 0;
+    dsssq(n/2, a, 2, &scale, &sumsq);
+    printf("scale=%20.15f, sumsq=%20.15f\n",scale,sumsq);
+    scale = 1;
+    sumsq = 0;
+    dsssq(n, a, 1, &scale, &sumsq);
+    printf("scale=%20.15f, sumsq=%20.15f\n",scale,sumsq);
+    scale = 1;
+    sumsq = 0;
+    dsssqvec(n, a, &scale, &sumsq);
+    printf("scale=%20.15f, sumsq=%20.15f\n",scale,sumsq);
+}
