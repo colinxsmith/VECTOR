d14cb819ded01e44dc9daa4d05a4f8260cc8c405
Colin <colinxsmith@gmail.com>
2021-01-17 16:39:47 +0000

    can do this

--- UseBlas/Program.cs
+++ UseBlas/Program.cs
@@ -9,14 +9,13 @@ namespace UseBlas
     {
         static void Main(string[] args)
         {
-            int n = 3;
             double a = 4;
             double[] x = { 1, 2, 3 };
             double[] y = { 4, 5, 6 };
-            BlasLike.daxpyvec(n, a, x, y);
-            for (int i = 0; i < n; ++i)
+            BlasLike.daxpyvec(x.Length, a, x, y);
+            for (int i = 0; i < x.Length; ++i)
             {
-                Console.WriteLine($"y[{i}]={y[i]}");
+                Console.WriteLine($"y[{i}]={y.GetValue(i)} {y[i]}");
             }
             bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
             if (isWindows) //Show how to read and write to Windows registry
