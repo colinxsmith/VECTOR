aaa4dedd2ecc1fbc127dca7f0399b561982ae42d
Colin <colinxsmith@gmail.com>
2021-01-25 22:32:59 +0000

    .

--- sssq.c
+++ sssq.c
@@ -90,7 +90,7 @@ int main()
         int back = 10;
         for (i = 0, ij = 0; i < n; ++i)
         {
-            printf("piv[%d] %d\n", i, piv[i]);
+            printf("piv[%d] %ld\n", i, piv[i]);
             for (j = 0; j <= i; ++j, ++ij)
             {
                 printf("a[%d][%d] %20.16f  ", i, j, a[ij]);
@@ -100,7 +100,7 @@ int main()
         back = dsptrf(U, n, a, piv);
         for (i = 0, ij = 0; i < n; ++i)
         {
-            printf("piv[%d] %d\n", i, piv[i]);
+            printf("piv[%d] %ld\n", i, piv[i]);
             for (j = 0; j <= i; ++j, ++ij)
             {
                 printf("a[%d][%d] %20.16f  ", i, j, a[ij]);
@@ -120,7 +120,7 @@ int main()
         double bb[] = {1, 2, 3};
         for (i = 0, ij = 0; i < n; ++i)
         {
-            printf("piv[%d] %d\n", i, piv[i]);
+            printf("piv[%d] %ld\n", i, piv[i]);
             for (j = i; j < n ; ++j, ++ij)
             {
                 printf("a[%d][%d] %20.16f  ", i, j, a[ij]);
@@ -131,7 +131,7 @@ int main()
         back = dsptrf(U, n, a, piv);
         for (i = 0, ij = 0; i < n; ++i)
         {
-            printf("piv[%d] %d\n", i, piv[i]);
+            printf("piv[%d] %ld\n", i, piv[i]);
             for (j = i; j < n ; ++j, ++ij)
             {
                 printf("a[%d][%d] %20.16f  ", i, j, a[ij]);
