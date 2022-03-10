7c0d84a4da36da9230c85425cb7c96348afca398
Colin Smith <colinxsmith@gmail.com>
2022-03-10 22:29:36 +0000

    we need featol much smaller than expected

--- BlasLike/Activeset.cs
+++ BlasLike/Activeset.cs
@@ -5074,7 +5074,7 @@ namespace ActiveSet
             var nctotl = n + m;
             var nrowa = m;
             double obj = 1e10;
-            var featolv = small_round(1e-10);
+            var featolv = small_round(1e-14);
             int cold = 1;
             short msglvl = -1000;
             opt.ISTATE = new int[n + m + n + n];
@@ -5111,7 +5111,7 @@ namespace ActiveSet
             var nctotl = n + m;
             var nrowa = m;
             double obj = 1e10;
-            var featolv = small_round(1e-10);
+            var featolv = small_round(1e-14);
             int cold = 1;
             short msglvl = -1000;
             opt.ISTATE = new int[n + m + n + n];
