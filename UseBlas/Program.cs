f1aeefbe1ad67b7e812818cf1ab093f17cc44181
Colin Smith <colinxsmith@gmail.com>
2022-08-05 21:12:10 +0100

    Update Program.cs

--- UseBlas/Program.cs
+++ UseBlas/Program.cs
@@ -15,7 +15,7 @@ namespace UseBlas
             {//How to make a file licence
                 var licence = new Licensing.Licence();
                 var now = new DateTimeOffset(DateTime.Now);
-var extra=now.Month>10?1:0;
+var extra=now.Month>=10?1:0;
                 var later = new DateTimeOffset(new DateTime(now.Year+extra, 10, 26, 12, 0, 0));
                 var testlicence = licence.licenceByteValue = new byte[20];
                 var hid = 0x13101955;
