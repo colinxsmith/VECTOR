32741034b7b20a528f2ffc69b08a25774ad5e040
Colin Smith <colinxsmith@gmail.com>
2023-01-20 16:29:03 +0000

    .

--- lvolid.cpp
+++ lvolid.cpp
@@ -24,17 +24,15 @@ int main()
     unsigned char mac_address[6];
     for (int i = 1; if_indextoname(i, getname); ++i)
     { // index starts at 1
-        ifr = ifc.ifc_req[i - 1];
-        printf("%d\t%s %s\n", i - 1, getname, ifr.ifr_name);
+        strcpy(ifr.ifr_name, getname);
         if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0)
         {
-            if (!(ifr.ifr_flags & IFF_LOOPBACK & IFF_RUNNING))
+            if (!(ifr.ifr_flags & IFF_LOOPBACK))
             { // don't count loopback
-                printf("%d\t%s %s\n", i - 1, getname, ifr.ifr_name);
                 if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0)
                 {
                     memcpy(mac_address, ifr.ifr_hwaddr.sa_data, 6);
-                    printf("Adapter %s (%s):\t%.2x%.2x%.2x%.2x%.2x%.2x\n", getname, ifr.ifr_name, mac_address[0], mac_address[1], mac_address[2], mac_address[3], mac_address[4], mac_address[5]);
+                    printf("Adapter %s:\t%.2x%.2x%.2x%.2x%.2x%.2x\n", ifr.ifr_name, mac_address[0], mac_address[1], mac_address[2], mac_address[3], mac_address[4], mac_address[5]);
                 }
             }
         }
