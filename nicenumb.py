8716f3d958744482fbde0c755ec6fd5c39f0925c
Dr Colin Smith <colin.smith@corfinancialgroup.com>
2023-06-13 15:21:30 +0100

    change nicenumb

--- nicenumb.py
+++ nicenumb.py
@@ -1,10 +1,10 @@
 from math import *
-def nicenumb(a):
-    delt=abs(a/10)
+def nicenumb(a,ticks=4):
+    delt=abs(a/ticks)
     mul=1
     if delt>1:
-        while delt>10:
-            delt/=10
+        while delt>ticks:
+            delt/=ticks
             mul-=1
         delt=round(delt)
         while mul<1:
@@ -13,14 +13,14 @@ def nicenumb(a):
         return delt
     else:
         while delt<1:
-            delt*=10
+            delt*=ticks
             mul+=1
         delt=round(delt)
         while mul>1:
             delt/=10
             mul-=1
         return delt
-for i in [1.23456789e3,1.23456789e2,1.23456789e1,1.23456789,1.23456789e-1,1.23456789e-2,1.23456789e-3,1.23456789e-4,1.23456789e-5]:
-    print ('%10f\t%f'%(i,nicenumb(i)))
-for i in [-1.23456789e3,-1.23456789e2,-1.23456789e1,-1.23456789,-1.23456789e-1,-1.23456789e-2,-1.23456789e-3,-1.23456789e-4,-1.23456789e-5]:
-    print ('%f\t%f'%(i,nicenumb(i)))
+k=-0.0857326569979281
+print(k,nicenumb(k))
+for d in [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]:
+    print('%f %f'% (d*nicenumb(k),k))
